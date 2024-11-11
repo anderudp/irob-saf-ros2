3ef866858c810729cfc24753e1abd0333f8272a9
anderudp <pappandrasdavid@gmail.com>
2024-11-11 21:17:05 +0000

    created utils, vision support pkgs

---
+++ irob_vision_support/irob_vision_support/fiducial_detector.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from irob_vision_support.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ irob_vision_support/irob_vision_support/hand_eye_registrator.py
@@ -0,0 +1,14 @@
+import rclpy
+from rclpy.node import Node
+import numpy as np
+
+import math
+from geometry_msgs.msg import PoseStamped, Pose
+from sensor_msgs.msg import JointState, Joy
+
+class HandEyeRegistrator(Node):
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
