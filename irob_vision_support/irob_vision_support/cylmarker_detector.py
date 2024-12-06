ede1a2143577e59af0362388f8f8684568e63d01
anderudp <pappandrasdavid@gmail.com>
2024-12-06 12:40:21 +0000

    fixed mistake in previous commit

--- irob_vision_support/irob_vision_support/cylmarker_detector.py
+++ irob_vision_support/irob_vision_support/cylmarker_detector.py
@@ -10,7 +10,6 @@ import os
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 from geometry_msgs.msg import PoseStamped
-import pyrealsense2.pyrealsense2 as rs
 from cv_bridge import CvBridge
 import matplotlib.pyplot as plt
 from cylmarker_utils.load_data import load_config_and_cam_calib_data, load_pttrn_and_marker_data
@@ -40,13 +39,11 @@ class CylmarkerDetector(Node):
         self.image_stamp: Time = None
         
 
-
     def estimate(self, image: np.ndarray):
         """Attempts pose estimation on the raw image,
         then publishes the result.
         """
 
-        # Estimate poses
         data_config, data_cam_calib = load_config_and_cam_calib_data(
             config_file_path=self.cylmarker_config_path, 
             cam_calib_file_path=self.cam_calib_config_path)
@@ -55,14 +52,7 @@ class CylmarkerDetector(Node):
             pattern_file_path=self.pattern_config_path,
             marker_file_data=self.marker_config_path)
 
-        # Realsense specific, use data_cam_calib for other cameras
-        dist_coeff_data = self.intrinsics.coeffs
-
-        cam_matrix = [[self.intrinsics.fx, 0, self.intrinsics.ppx],
-                      [0, self.intrinsics.fy, self.intrinsics.ppy],
-                      [0, 0, 1]]
-        
-        pose_pred = pose_estimation.estimate_poses(image, cam_matrix, dist_coeff_data, data_config, data_pattern, data_marker)
+        pose_pred = pose_estimation.estimate_poses(image, data_cam_calib, data_config, data_pattern, data_marker)
 
         p = PoseStamped()
         q = quaternion_from_matrix(pose_pred)
@@ -73,10 +63,12 @@ class CylmarkerDetector(Node):
         p.pose.position.x = pose_pred[0][3]
         p.pose.position.y = pose_pred[1][3]
         p.pose.position.z = pose_pred[2][3]
-        p.header.stamp = self.latest_stamp
+        p.header.stamp = self.image_stamp
         self.cylmarker_tf_pub.publish(p)
 
-    def take_photo(self, save_raw: bool = False) -> np.ndarray:
+
+    def take_photo(self, save_raw: bool = False):
+        
         # Attempt to take a photo
         capture = cv2.VideoCapture(0)
         success = False
